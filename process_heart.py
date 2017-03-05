0f9c094834951826aaa5fee2ba053e69b4321b38
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-03-04 20:06:19 -0600

    Add improved heart rate data processing

--- process_heart.py
+++ process_heart.py
@@ -7,12 +7,14 @@ for i in first_heart_data[1:len(first_heart_data)-2]:
 
 highs = []
 lows = []
+low_high = []
 lastDay = ""
 index = -1
 maxHigh = 0
 maxLow = 200
 for i in heart_data:
     if not i[0][0:10] == lastDay and not index == -1:
+        low_high += [[lastDay, maxLow, maxHigh]]
         highs += [maxHigh]
         lows += [maxLow]
     if not i[0][0:10] == lastDay:
@@ -24,3 +26,10 @@ for i in heart_data:
     if int(i[1]) < maxLow:
         maxLow = i[1]
     lastDay = i[0][0:10]
+
+low_high_file = open('heart/low_high.csv', 'w')
+toOutput = 'date,low,high\n'
+for i in low_high:
+    toOutput += str(i[0]) + ',' + str(i[1]) + ',' + str(i[2]) + '\n'
+low_high_file.write(toOutput)
+low_high_file.close()
